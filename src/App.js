0b219255a3996e0d44b42b54784e6ff63d8ea738
Maud <maud.aufevre@gmail.com>
2020-07-28 15:23:15 +0200

    1ere version blog9aout

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css';
+
+
+function App() {
+  return (
+    <Fragment>
+      <div className="">
+      <Header/>
+      <Footer/>
+      
+      </div>  
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from './Nav'
+
+// raccourcis clavier pour crééer un composant fonction : scf
+const Header = () => {
+    return ( 
+        <div></div>
+     );
+}
+ 
+export default Header;
